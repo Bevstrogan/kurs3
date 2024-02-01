446ec7af960eb2afa1c8e11d3e677e9a8c76b92e
bevstrogan <bevstrogan@gmail.com>
2024-02-01 16:31:29 +0300

    Написаны тесты?

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import json
-from src.utils import display_last_operations
+from src.utils import display_last_operations, show_operations
 
 ''' Главный код функции '''
 if __name__ == "__main__":
     with open('operations.json', 'r', encoding='utf-8') as json_file:
         file = json.load(json_file)
-        display_last_operations(file)
+        last_operations = display_last_operations(file)
+        show_operations(last_operations)

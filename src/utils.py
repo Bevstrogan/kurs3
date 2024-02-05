f2fcb8f7e1c311acd923506040d34d0c6eb991c5
дмитрий алехин <bevstrogan@gmail.com>
2024-02-05 16:24:16 +0300

    Доработки, исправление тестов

--- src/utils.py
+++ src/utils.py
@@ -6,29 +6,32 @@ class Operation():
         self.operation = operation
 
     def __repr__(self):
-        return f'{self.operation}'
+        '''Выводит информацию об операции'''
+        return f"{self.__class__.__name__}('{self.operation}')"
 
     def show_description(self):
+        '''Выподит описание операции'''
         return self.operation['description']
 
     def show_from(self):
+        '''Выводит от кого пришел платеж'''
         if "from" in self.operation.keys():
             return self.operation['from']
         else:
             return ''
 
     def show_to(self):
+        '''Выводит куда пришел платеж'''
         return self.operation["to"]
 
     def date_format(self):
-        """
-        Выводит дату
-        """
+        '''Выводит дату совершения операции'''
         operation_date_str = self.operation["date"]
         operation_date = datetime.datetime.strptime(operation_date_str, "%Y-%m-%dT%H:%M:%S.%f")
         return operation_date.strftime("%d.%m.%Y")
 
     def censore_number(self, account):
+        '''Функция скрывает номер карты/счета'''
         account = account.split(" ")
         account_number = account[-1]
         account.pop(len(account) - 1)
@@ -43,11 +46,13 @@ class Operation():
 
 
 def load_operations(path):
+    '''Функция открывает и загружает json файл'''
     with open(path, "r", encoding='utf8') as file:
         operations = json.load(file)
         return operations
 
 def last_five_operations(operations):
+    '''Функция сортирует и возвращает 5 операций соответствующим условиям'''
     oper_list_clean = [opr for opr in operations if opr != {} and opr["state"] == "EXECUTED"]
     oper_list_clean.sort(key=lambda dictionary: dictionary["date"], reverse=True)
     last_five_oper = oper_list_clean[0:5]
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,28 +1,60 @@
-from utils_for_test import *
 import pytest
 
+from src.utils import *
 
 @pytest.fixture
-def fixture_1():
-    operation_test_1 = {'date': '2000-12-08T22:46:21.935582',
-                            'operationAmount': {'amount': '41096.24',
-                                                'currency': {'name': 'USD', 'code': 'USD'}},
-                            'description': 'Открытие вклада',
-                            'to': 'Счет 90424923579946435907'}
-    return operation_test_1
+def class_fixture1():
+    operation_test = Operation({'date': '2000-12-08T22:46:21.935582',
+                                'operationAmount': {'amount': '41096.24',
+                                                    'currency': {'name': 'USD', 'code': 'USD'}},
+                                'description': 'Открытие вклада',
+                                'to': 'Счет 90424923579946435907'})
+    return operation_test
 
 @pytest.fixture
-def fixture_2():
-    operation_test_2 = {'date': '2001-11-19T09:22:25.899614',
-                            'operationAmount': {'amount': '30153.72',
-                                                'currency': {'name': 'руб.', 'code': 'RUB'}},
-                            'description': 'Перевод организации',
-                            'from': 'Maestro 7810846596785568',
-                            'to': 'MasterCard 6783917276771847'}
+def class_fixture2():
+    operation_test_2 = Operation({'date': '2011-10-01T09:22:25.899614',
+                                  'operationAmount': {'amount': '30153.72',
+                                                      'currency': {'name': 'руб.', 'code': 'RUB'}},
+                                  'description': 'Перевод организации',
+                                  'from': 'Maestro 7810846596785568',
+                                  'to': 'MasterCard 6783917276771847'})
     return operation_test_2
-def test_date_format(fixture_1):
-    assert date_format(fixture_1) == "08.12.2000"
 
+def test___repr__(class_fixture1):
+    assert class_fixture1.__repr__() ==  "Operation('{'date': '2000-12-08T22:46:21.935582', " \
+                                         "'operationAmount': {'amount': '41096.24', " \
+                                         "'currency': {'name': 'USD', 'code': 'USD'}}, " \
+                                         "'description': 'Открытие вклада', " \
+                                         "'to': 'Счет 90424923579946435907'}')"
+def test_date_format(class_fixture1):
+    assert class_fixture1.date_format() == '08.12.2000'
 
+def test_show_description(class_fixture1):
+    assert class_fixture1.show_description() == 'Открытие вклада'
 
+def test_show_from_none(class_fixture1):
+    assert class_fixture1.show_from() == ''
 
+def test_show_from_has(class_fixture2):
+    assert class_fixture2.show_from() == 'Maestro 7810846596785568'
+
+def test_censore_number_card(class_fixture2):
+    assert class_fixture2.censore_number(class_fixture2.show_from()) == 'Maestro 7810 84** **** 5568'
+
+def test_censore_number_account(class_fixture1):
+    assert class_fixture1.censore_number(class_fixture1.show_to()) == "Счет **5907"
+
+def test_last_five_operations():
+    operations = [{"id": "1", "date": "01.01.2022", "state": "EXECUTED"},
+                {},
+                {"id": "2", "date": "03.01.2022", "state": "EXECUTED"},
+                {"id": "3", "date": "06.01.2022", "state": "EXECUTED"},
+                {"id": "4", "date": "10.01.2022", "state": "CANCELED"},
+                {"id": "5", "date": "15.01.2022", "state": "EXECUTED"},
+                {"id": "6", "date": "21.01.2022", "state": "EXECUTED"}]
+    assert last_five_operations(operations) == [{'date': '21.01.2022', 'id': '6', 'state': 'EXECUTED'},
+                                                {'date': '15.01.2022', 'id': '5', 'state': 'EXECUTED'},
+                                                {'date': '06.01.2022', 'id': '3', 'state': 'EXECUTED'},
+                                                {'date': '03.01.2022', 'id': '2', 'state': 'EXECUTED'},
+                                                {'date': '01.01.2022', 'id': '1', 'state': 'EXECUTED'}]
